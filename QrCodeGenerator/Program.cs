9def18ed41be0cd6eb02c5fcc55b4c3f35d5262a
Marco Minerva <marco.minerva@gmail.com>
2025-11-28 17:12:32 +0100

    Add project files.

---
+++ QrCodeGenerator/Program.cs
@@ -0,0 +1,122 @@
+using System.ComponentModel;
+using System.Net.Mime;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Net.Http.Headers;
+using QRCoder;
+using Scalar.AspNetCore;
+using TinyHelpers.AspNetCore.Extensions;
+using TinyHelpers.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("QrCodeGenerationRateLimiter", context =>
+    {
+        return RateLimitPartition.GetFixedWindowLimiter(context.Connection.RemoteIpAddress?.ToString() ?? "unknown", _ =>
+            new()
+            {
+                PermitLimit = 50,
+                Window = TimeSpan.FromMinutes(1),
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 0
+            });
+    });
+
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    options.OnRejected = (context, _) =>
+    {
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var window))
+        {
+            var response = context.HttpContext.Response;
+            response.Headers.RetryAfter = window.TotalSeconds.ToString();
+        }
+
+        return ValueTask.CompletedTask;
+    };
+});
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddPolicy("QrCodeGenerationCachePolicy", policy =>
+    {
+        policy.Expire(TimeSpan.FromHours(1));
+    });
+});
+
+builder.Services.AddDefaultProblemDetails();
+builder.Services.AddDefaultExceptionHandler();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(_ => true)
+            .AllowCredentials().WithExposedHeaders(HeaderNames.ContentDisposition);
+    });
+});
+
+builder.Services.AddOpenApi(options =>
+{
+    options.RemoveServerList();
+    options.AddDefaultProblemDetailsResponse();
+    options.DescribeAllParametersInCamelCase();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseForwardedHeaders(new()
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+    KnownProxies = { }
+});
+
+app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
+
+app.MapOpenApi();
+
+app.MapScalarApiReference();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "QrCodeGenerator API V1");
+});
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseCors();
+
+app.UseOutputCache();
+app.UseRateLimiter();
+
+app.MapGet("/qrcode", ([AsParameters] QrCodeSettings qrCodeSettings) =>
+{
+    var qrCodeImage = PngByteQRCodeHelper.GetQRCode(qrCodeSettings.Content, QRCodeGenerator.ECCLevel.Q,
+        qrCodeSettings.Size.GetValueOrDefault(3), qrCodeSettings.DrawBorder.GetValueOrDefault(true));
+
+    return TypedResults.File(qrCodeImage, MediaTypeNames.Image.Png);
+})
+.Produces(StatusCodes.Status200OK, contentType: MediaTypeNames.Image.Png)
+.CacheOutput("QrCodeGenerationCachePolicy")
+.RequireRateLimiting("QrCodeGenerationRateLimiter");
+
+app.Run();
+
+public class QrCodeSettings
+{
+    public required string Content { get; set; }
+
+    [DefaultValue(3)]
+    public int? Size { get; set; }
+
+    [DefaultValue(true)]
+    public bool? DrawBorder { get; set; }
+}
